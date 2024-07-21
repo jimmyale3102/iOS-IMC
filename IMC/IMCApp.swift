211d65b67e71a7a0a2af87bc89dc06d7677cd6ac
jimmyale3102 <jimmyale3102@gmail.com>
2024-07-22 09:50:22 +1000

    Initial Commit

---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Jimmy on 22/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
