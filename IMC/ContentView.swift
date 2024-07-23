8e6bb96278804e49d3db7a0faa31f9c7dc41fa7d
jimmyale3102 <jimmyale3102@gmail.com>
2024-07-23 22:34:20 +1000

    Add Gender buttons

--- IMC/ContentView.swift
+++ IMC/ContentView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let MAN: Int = 0
+    let WOMAN: Int = 1
+    @State var genderSelected: Int = 0 
+    
     var body: some View {
         NavigationView {
             VStack {
-                
+                HStack {
+                    GenderButton(
+                        iconName: "heart.fill",
+                        text: "Mujer",
+                        gender: WOMAN,
+                        genderSelected: $genderSelected
+                    )
+                    GenderButton(
+                        iconName: "star.fill",
+                        text: "Hombre",
+                        gender: MAN,
+                        genderSelected: $genderSelected
+                    )
+                }.frame(height: 120)
             }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+                .padding()
                 .background(.backgroundApp)
                 .toolbar {
                     ToolbarItem(placement: .principal) {
@@ -20,6 +39,48 @@ struct ContentView: View {
                     }
                 }
         }
+    }
+}
+
+struct GenderButton: View {
+    let iconName: String
+    let text: String
+    let gender: Int
+    @Binding var genderSelected: Int
+    
+    var body: some View {
+        
+        let backgroundColor = if (gender == genderSelected) {
+            Color.onSelected
+        } else {
+            Color.backgroundComponent
+        }
+        
+        Button(action: {
+            genderSelected = gender
+        }) {
+            VStack {
+                Image(systemName: iconName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 42)
+                    .foregroundColor(.white)
+                DescriptionText(text: text)
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .background(backgroundColor)
+            .cornerRadius(12)
+        }
+    }
+}
+
+struct DescriptionText: View {
+    let text: String
+    var body: some View {
+        Text(text)
+            .font(.title3)
+            .bold()
+            .foregroundColor(.white)
     }
 }
 
